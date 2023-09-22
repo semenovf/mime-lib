87ccdc6f09c7404588f3d4ca070b294285e0f13b
Vladislav Trifochkin <demoversion@mail.ru>
2023-09-22 07:55:16 +0500

    fix: Fixed `mime_by_extension` tests.

--- tests/mime_by_extension.cpp
+++ tests/mime_by_extension.cpp
@@ -16,10 +16,36 @@ namespace fs = pfs::filesystem;
 
 TEST_CASE("mime_by_extension") {
 
-    CHECK(mime::mime_by_extension(fs::path{"image.jpeg"}) == mime::mime_enum::image__jpeg);
-    CHECK(mime::mime_by_extension(fs::path{"image.jpg"}) == mime::mime_enum::image__jpeg);
-    CHECK(mime::mime_by_extension(fs::path{"image.JPEG"}) == mime::mime_enum::image__jpeg);
-    CHECK(mime::mime_by_extension(fs::path{"image.jPg"}) == mime::mime_enum::image__jpeg);
+    struct {
+        char const * filename;
+        mime::mime_enum expected;
+    } test_data[] = {
+          { "text.txt"  , mime::mime_enum::text__plain }
+        , { "text.tXt"  , mime::mime_enum::text__plain }
+        , { "text.LOG"  , mime::mime_enum::text__plain }
+        , { "site.html" , mime::mime_enum::text__html }
+        , { "SITE.HTM"  , mime::mime_enum::text__html }
+        , { "audio.ogg" , mime::mime_enum::audio__ogg }
+        , { "audio.Wav" , mime::mime_enum::audio__wav }
+        , { "video.MP4" , mime::mime_enum::video__mp4 }
+        , { "image.BMP" , mime::mime_enum::image__bmp }
+        , { "image.GIF" , mime::mime_enum::image__gif }
+        , { "image.ICO" , mime::mime_enum::image__vnd_microsoft_icon }
+        , { "image.png" , mime::mime_enum::image__png }
+        , { "image.TIFF", mime::mime_enum::image__tiff }
+        , { "image.tif" , mime::mime_enum::image__tiff }
+        , { "image.jpeg", mime::mime_enum::image__jpeg }
+        , { "image.jpg" , mime::mime_enum::image__jpeg }
+        , { "image.JPEG", mime::mime_enum::image__jpeg }
+        , { "image.jPg" , mime::mime_enum::image__jpeg }
 
-    CHECK(mime::mime_by_extension(fs::path{"file.abracadabra"}) == mime::mime_enum::unknown);
+        , { "", mime::mime_enum::unknown }
+        , { "app", mime::mime_enum::unknown }
+        , { "app.unknown", mime::mime_enum::unknown }
+        , { "file.abracadabra", mime::mime_enum::unknown }
+    };
+
+    for (auto const elem: test_data) {
+        CHECK_EQ(mime::mime_by_extension(fs::utf8_decode(elem.filename)), elem.expected);
+    }
 }
